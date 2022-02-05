1466d415d42d84cb91bb124ec7179fd1b06143cd
imHarish03 <harishhari302@gmail.com>
2022-02-05 18:45:49 +0530

    first commit

---
+++ src/main/java/lab/imHarish03/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package lab.imHarish03.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/imHarish03/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.imHarish03.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
